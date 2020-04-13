16e63fe45360bcdc7371fab5bcc2d652f064590b
Pepper <drgreen7@asu.edu>
2020-04-12 20:26:13 -0700

    added player and monster classes

--- dnd.py
+++ dnd.py
@@ -6,8 +6,98 @@ import random
 import csv
 
 
-# enemy class
-# player class
+# creature class, parent class to player classes and monster
+class Creature:
+    def __init__(self, name):
+        self.name = name
+
+    # setters
+    def setHp(self, hp):
+        self.hp = hp
+
+    def setAc(self, ac):
+        self.ac = ac
+
+    def setToHit(self, toHit):
+        self.toHit = toHit
+
+    def setDmgDie(self, die):
+        self.dmgDie = die
+
+    def setDmgBonus(self, bonus):
+        self.dmgBonus = bonus
+
+    def setsaveBonus(self, bonus):
+        self.saveBonus = bonus
+
+
+# Monster class inherits from Creature
+class Monster(Creature):
+    def __init__(self, name):
+        super().__init__(name)
+
+
+# Player classes inherit from Creature
+class Fighter(Creature):
+    def __init__(self, name):
+        super().__init__(name)
+        self.maxHp = 38
+        self.hp = 38
+        self.ac = 16
+        self.meleeToHit = 7
+        self.meleeDmgDie = 10
+        self.meleeDmgBonus = 5
+        self.rangedToHit = 6
+        self.rangedDmgDie = 8
+        self.rangedDmgBonus = 4
+
+    def attack(self, attackType):
+        if(attackType == "melee"):
+            damageDealt = random.randint(0, self.meleeDmgDie) + self.meleeDmgBonus
+            return damageDealt
+        elif(attackType == "ranged"):
+            damageDealt = random.randint(0, self.rangedDmgDie) + self.rangedDmgBonus
+            return damageDealt
+        else:
+            return 0
+
+
+class Sorcerer(Creature):
+    def __init__(self, name):
+        super().__init__(name)
+        self.maxHp = 28
+        self.hp = 28
+        self.ac = 15
+        self.toHit = 4
+        self.dmgDie = 6
+        self.dmgBonus = 2
+        # setting limits to the powerful spells
+        self.maxFire = 3
+        self.maxFear = 1
+        self.fireRemaining = self.maxFire
+        self.fearRemaining = self.maxFear
+
+    # sorcerer has spells available
+    def spellcast(self, spell):
+        if(spell == "firebolt"):
+            # a single fire bolt does 1d10
+            return random.randint(0, 10)
+        if(spell == "fireball"):
+            if(self.fireRemaining == 0):
+                return 0
+            else:
+                self.fireRemaining = self.fireRemaining - 1
+                # fireballs do 8d6
+                sum = 0
+                for i in range(0, 8):
+                    sum = sum + random.randint(0,6)
+                return sum
+        if(spell == "fear"):
+            if(self.fearRemaining == 0):
+                print("You have run out of Fireballs to use! Take a rest to regain")
+                return 0
+            else:
+                self.fearRemaining = self.fearRemaining - 1
 
 # declare a pc outside of any functions?
 
@@ -59,8 +149,8 @@ def worldMove():
     return
 
 
-def rollInitiative(hp, ac, speed, toHit, damageDie, damageBonus,
-                    inMap, pcPos1, pcPos2, monPos1, monPos2, name):
+def rollInitiative(hp, ac, toHit, damageDie, damageBonus,
+                    inMap, pcPos1, pcPos2, monPos1, monPos2, name, save, player):
     # do the battle, printing to stdout
     # --------------instantiate a monster object here-----------------
     # make a grid for the battle map
@@ -82,6 +172,29 @@ def rollInitiative(hp, ac, speed, toHit, damageDie, damageBonus,
         for h in g:
             print(''.join(h), " ", end = "")
         print()
+    print(player.spellcast("fireball"))
+
+    # # testing
+    # mon = Monster("skeleton")
+    # # print(mon.name)
+    # mon.setHp(20)
+    # print(mon.hp)
+    # mon.setHp(mon.hp - 4)
+    # print(mon.hp)
+
+    # zombie = Monster("zombie")
+    # zombie.setHp(50)
+    # # print(zombie.hp)
+    # # print(mon.hp)
+
+    # sorc = Sorcerer("Phillip")
+    # # print(sorc.hp, " ", sorc.fireRemaining)
+    # print(sorc.spellcast("firebolt"))
+
+    # jane = Fighter("Jane")
+    # print(jane.hp)
+    # print(jane.attack("melee"))
+
 
     # battle loop:
     # player and enemy drawn on map
@@ -105,7 +218,8 @@ def main():
     world = worldMap()
     a = 0
     b = 0
-    # declare playerCharacter object, assign values <-- should this happen outside of function?
+    maple = Sorcerer("Maple")
+    print(maple.hp)
     # while (winState != 1)
     # (win state will become 1 in a specific encounter)
     pos = world[a][b]
@@ -117,10 +231,12 @@ def main():
         # armor upgrade
     if(pos[0] == "encounter"):
         # I will end up handling these arguments by simply passing a monster object
-        rollInitiative(pos[1], pos[2], pos[3], pos[4], pos[5], pos[6], pos[7], pos[8], pos[9], pos[10], pos[11], pos[11])
+        rollInitiative(pos[1], pos[2], pos[3], pos[4], pos[5], pos[6], pos[7], pos[8], pos[9], pos[10], pos[11], pos[12], maple)
         # the encounter may give a key item to the player, or the location of the goal (winstate trigger)
+        # I should set a trigger to know if this encounter has happened already,
+        # in the case the player back tracks
     #if the player is still alive after the encounter...
-    worldMove()
+    #worldMove()
 
     return
 
